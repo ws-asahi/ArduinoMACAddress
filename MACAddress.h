f6e726508ed8ff90e5493fb50e218520529e6d92
朝日工房 <yusuke.shimizu.00@outlook.com>
2026-03-09 01:15:32 +0900

    add support to common mac address text formats
    old source code supported only separator ':' and 6 bytes.
    
    new source code supported 6 format
    1.Upper hex ,':' separator, and 6 bytes
    2.Lower hex and ':' separator, and 6 bytes
    3.Upper hex and '-' separator, and 6 bytes
    4.Lower hex and '-' separator, and 6 bytes
    5.Upper hex and '.' separator, and 3 words
    6.Lower hex and '.' separator, and 3 words

--- MACAddress.h
+++ MACAddress.h
@@ -1,145 +1,359 @@
+//-----------------------------------------------------------------
+//
+//  MAC Address(EUI-48)
+//
+//  Auther: Yusuke Shimizu - Workshpop Asahi
+//  https://www.ws-asahi.net
+//
+//-----------------------------------------------------------------
 #pragma once
-#ifndef _MAC_ADDRESS_H_
-#define _MAC_ADDRESS_H_
+#ifndef MAC_ADDRESS_H
+#define MAC_ADDRESS_H
+#include <Arduino.h>
 
-#include <stdint.h>
-#include <Printable.h>
-#include <String.h>
+#define MAC_FORMAT_UPPER_COLON "%02X:%02X:%02X:%02X:%02X:%02X"
+#define MAC_FORMAT_LOWER_COLON "%02x:%02x:%02x:%02x:%02x:%02x"
+#define MAC_FORMAT_UPPER_HYPHEN "%02X-%02X-%02X-%02X-%02X-%02X"
+#define MAC_FORMAT_LOWER_HYPHEN "%02x-%02x-%02x-%02x-%02x-%02x"
+#define MAC_FORMAT_UPPER_PERIOD "%04X.%04X.%04X"
+#define MAC_FORMAT_LOWER_PERIOD "%04x.%04x.%04x"
 
-#define MAC_BYTES_INDEX 12
+#define MAC_BYTES_LENGTH 6
+#define MAC_WORDS_LENGTH 3
+#define MAC_STR_LENGTH 32
 
 class MACAddress : public Printable {
+public:
+  enum MacFormat {
+    UPPER_COLON,  // default setting
+    LOWER_COLON,
+    UPPER_HYPHEN,
+    LOWER_HYPHEN,
+    UPPER_PERIOD,
+    LOWER_PERIOD,
+  };
+
 private:
   union {
-    uint8_t bytes[6];
+    uint8_t bytes[MAC_BYTES_LENGTH];
+    uint16_t words[MAC_WORDS_LENGTH];
   } _address;
+
+  static inline MacFormat _defaultFormat = MacFormat::UPPER_COLON;
+  MacFormat _format;
 
 public:
   MACAddress() {
-    memset(_address.bytes, 0, sizeof(_address.bytes));
+    clear();
+    _format = _defaultFormat;
   }
-  MACAddress(uint8_t first_octet, uint8_t second_octet, uint8_t third_octet, uint8_t fourth_octet, uint8_t fivth_octet, uint8_t sixth_octet) {
-    _address.bytes[0] = first_octet;
-    _address.bytes[1] = second_octet;
-    _address.bytes[2] = third_octet;
-    _address.bytes[3] = fourth_octet;
-    _address.bytes[4] = fivth_octet;
-    _address.bytes[5] = sixth_octet;
+  MACAddress(const MacFormat format) {
+    clear();
+    _format = format;
   }
-  MACAddress(const uint8_t *address) {
-    memcpy(_address.bytes, address, sizeof(_address.bytes));
+  MACAddress(const uint8_t firstOctet, const uint8_t secondOctet, const uint8_t thirdOctet, const uint8_t fourthOctet, const uint8_t fifthOctet, const uint8_t sixthOctet) {
+    fromBytes(firstOctet, secondOctet, thirdOctet, fourthOctet, fifthOctet, sixthOctet);
+    _format = _defaultFormat;
   }
-  MACAddress(const char *address) {
-    fromString(address);
+  MACAddress(const uint8_t firstOctet, const uint8_t secondOctet, const uint8_t thirdOctet, const uint8_t fourthOctet, const uint8_t fifthOctet, const uint8_t sixthOctet, const MacFormat format) {
+    fromBytes(firstOctet, secondOctet, thirdOctet, fourthOctet, fifthOctet, sixthOctet);
+    _format = format;
+  }
+  MACAddress(const uint16_t firstWord, const uint16_t secondWord, const uint16_t thirdWord) {
+    fromWords(firstWord, secondWord, thirdWord);
+    _format = _defaultFormat;
+  }
+  MACAddress(const uint16_t firstWord, const uint16_t secondWord, const uint16_t thirdWord, const MacFormat format) {
+    fromWords(firstWord, secondWord, thirdWord);
+    _format = format;
+  }
+  MACAddress(const uint8_t* addr) {
+    fromBytes(addr);
+    _format = _defaultFormat;
+  }
+  MACAddress(const uint8_t* addr, const MacFormat format) {
+    fromBytes(addr);
+    _format = format;
+  }
+  MACAddress(const uint16_t* addr) {
+    fromWords(addr);
+    _format = _defaultFormat;
+  }
+  MACAddress(const uint16_t* addr, const MacFormat format) {
+    fromWords(addr);
+    _format = format;
+  }
+  MACAddress(const MACAddress& addr) {
+    fromBytes(addr._address.bytes);
+    _format = _defaultFormat;
+  }
+  MACAddress(const MACAddress& addr, const MacFormat format) {
+    fromBytes(addr._address.bytes);
+    _format = format;
+  }
+  MACAddress(const char* addr) {
+    fromString(addr);
+    _format = _defaultFormat;
+  }
+  MACAddress(const char* addr, const MacFormat format) {
+    fromString(addr);
+    _format = format;
+  }
+  MACAddress(const String& addr) {
+    fromString(addr);
+    _format = _defaultFormat;
+  }
+  MACAddress(const String& addr, const MacFormat format) {
+    fromString(addr);
+    _format = format;
   }
 
-  bool fromString(const char *address) {
-    uint8_t dots = 0;
-    uint8_t hexs = 0;
-    uint16_t acc = 0;
+  bool fromString(const char* addr) {
+    uint8_t newBytes[MAC_BYTES_LENGTH];
+    uint16_t newWords[MAC_WORDS_LENGTH];
 
-    memset(_address.bytes, 0, sizeof(_address.bytes));
+    int result = 0;
+    result = sscanf(addr, MAC_FORMAT_UPPER_COLON, &newBytes[0], &newBytes[1], &newBytes[2], &newBytes[3], &newBytes[4], &newBytes[5]);
+    if (result == MAC_BYTES_LENGTH) {
+      fromBytes(newBytes);
+      return true;
+    }
+    result = sscanf(addr, MAC_FORMAT_LOWER_COLON, &newBytes[0], &newBytes[1], &newBytes[2], &newBytes[3], &newBytes[4], &newBytes[5]);
+    if (result == MAC_BYTES_LENGTH) {
+      fromBytes(newBytes);
+      return true;
+    }
 
-    while (*address) {
-      char c = *address;
-      acc = 0;
-      if ('0' <= c && c <= '9') {
-        acc = acc << 4;
-        acc += *address - '0';
-        hexs++;
-      } else if ('a' <= c && c <= 'f') {
-        acc = acc << 4;
-        acc += *address - 'a';
-        hexs++;
-      } else if ('A' <= c && c <= 'F') {
-        acc = acc << 4;
-        acc += *address - 'A';
-        hexs++;
-      } else if (c == ':') {
-        if (hexs != 2) {
-          return false;
-        } else if (!(0x00 <= acc && acc <= 0xFF)) {
-          return false;
-        } else {
-          _address.bytes[dots++] = (uint8_t)acc;
-          hexs = 0;
-          acc = 0;
+    result = sscanf(addr, MAC_FORMAT_UPPER_HYPHEN, &newBytes[0], &newBytes[1], &newBytes[2], &newBytes[3], &newBytes[4], &newBytes[5]);
+    if (result == MAC_BYTES_LENGTH) {
+      fromBytes(newBytes);
+      return true;
+    }
+    result = sscanf(addr, MAC_FORMAT_LOWER_HYPHEN, &newBytes[0], &newBytes[1], &newBytes[2], &newBytes[3], &newBytes[4], &newBytes[5]);
+    if (result == MAC_BYTES_LENGTH) {
+      fromBytes(newBytes);
+      return true;
+    }
+
+    result = sscanf(addr, MAC_FORMAT_UPPER_PERIOD, &newWords[0], &newWords[1], &newWords[2]);
+    if (result == MAC_WORDS_LENGTH) {
+      fromWords(newWords);
+      return true;
+    }
+    result = sscanf(addr, MAC_FORMAT_LOWER_PERIOD, &newWords[0], &newWords[1], &newWords[2]);
+    if (result == MAC_WORDS_LENGTH) {
+      fromWords(newWords);
+      return true;
+    }
+
+    return false;
+  }
+  bool fromString(const char* addr, const MacFormat format) {
+    uint8_t newBytes[MAC_BYTES_LENGTH];
+    uint16_t newWords[MAC_WORDS_LENGTH];
+
+    int result = 0;
+    switch (format) {
+      case MacFormat::UPPER_COLON:
+        result = sscanf(addr, MAC_FORMAT_UPPER_COLON, &newBytes[0], &newBytes[1], &newBytes[2], &newBytes[3], &newBytes[4], &newBytes[5]);
+        if (result == MAC_BYTES_LENGTH) {
+          fromBytes(newBytes);
+          return true;
         }
-      }
+        break;
+
+      case MacFormat::LOWER_COLON:
+        result = sscanf(addr, MAC_FORMAT_LOWER_COLON, &newBytes[0], &newBytes[1], &newBytes[2], &newBytes[3], &newBytes[4], &newBytes[5]);
+        if (result == MAC_BYTES_LENGTH) {
+          fromBytes(newBytes);
+          return true;
+        }
+        break;
+
+      case MacFormat::UPPER_HYPHEN:
+        result = sscanf(addr, MAC_FORMAT_UPPER_HYPHEN, &newBytes[0], &newBytes[1], &newBytes[2], &newBytes[3], &newBytes[4], &newBytes[5]);
+        if (result == MAC_BYTES_LENGTH) {
+          fromBytes(newBytes);
+          return true;
+        }
+        break;
+
+      case MacFormat::LOWER_HYPHEN:
+        result = sscanf(addr, MAC_FORMAT_LOWER_HYPHEN, &newBytes[0], &newBytes[1], &newBytes[2], &newBytes[3], &newBytes[4], &newBytes[5]);
+        if (result == MAC_BYTES_LENGTH) {
+          fromBytes(newBytes);
+          return true;
+        }
+        break;
+
+      case MacFormat::UPPER_PERIOD:
+        result = sscanf(addr, MAC_FORMAT_UPPER_PERIOD, &newWords[0], &newWords[1], &newWords[2]);
+        if (result == MAC_WORDS_LENGTH) {
+          fromWords(newWords);
+          return true;
+        }
+        break;
+
+      case MacFormat::LOWER_PERIOD:
+        result = sscanf(addr, MAC_FORMAT_LOWER_PERIOD, &newWords[0], &newWords[1], &newWords[2]);
+        if (result == MAC_WORDS_LENGTH) {
+          fromWords(newWords);
+          return true;
+        }
+        break;
     }
 
-    if (hexs > 2) {
-      return false;
-    }
-    if (hexs != 2) {
-      return false;
-    } else if (!(0x00 <= acc && acc <= 0xFF)) {
-      return false;
-    }
-
-    if (dots != 5) {
-      return false;
-    }
-
-    _address.bytes[5] = (uint8_t)acc;
-    return true;
+    return false;
   }
 
-  bool fromString(const String &address) {
-    return fromString(address.c_str());
+  bool fromString(const String& addr) {
+    char str[MAC_STR_LENGTH];
+    addr.toCharArray(str, MAC_STR_LENGTH);
+    return fromString(str);
+  }
+  bool fromString(const String& addr, const MacFormat format) {
+    char str[MAC_STR_LENGTH];
+    addr.toCharArray(str, MAC_STR_LENGTH);
+    return fromString(str, format);
   }
 
-  bool equals(const MACAddress &addr) const {
+  void fromBytes(const uint8_t firstOctet, const uint8_t secondOctet, const uint8_t thirdOctet, const uint8_t fourthOctet, const uint8_t fifthOctet, const uint8_t sixthOctet) {
+    _address.bytes[0] = firstOctet;
+    _address.bytes[1] = secondOctet;
+    _address.bytes[2] = thirdOctet;
+    _address.bytes[3] = fourthOctet;
+    _address.bytes[4] = fifthOctet;
+    _address.bytes[5] = sixthOctet;
+  }
+  void fromBytes(const uint8_t* addr) {
+    memcpy(_address.bytes, addr, MAC_BYTES_LENGTH);
+  }
+  void fromWords(const uint16_t firstWord, const uint16_t secondWord, const uint16_t thirdWord) {
+    _address.words[0] = firstWord;
+    _address.words[1] = secondWord;
+    _address.words[2] = thirdWord;
+  }
+  void fromWords(const uint16_t* addr) {
+    memcpy(_address.words, addr, MAC_WORDS_LENGTH);
+  }
+
+  bool equals(const MACAddress& addr) const {
     return _address.bytes[0] == addr[0] && _address.bytes[1] == addr[1] && _address.bytes[2] == addr[2] && _address.bytes[3] == addr[3] && _address.bytes[4] == addr[4] && _address.bytes[5] == addr[5];
   }
+  bool equals(const uint8_t* addr) const {
+    return equals(addr[0], addr[1], addr[2], addr[3], addr[4], addr[5]);
+  }
+  bool equals(const uint16_t* addr) const {
+    return equals(addr[0], addr[1], addr[2]);
+  }
+  bool equals(const uint8_t firstOctet, const uint8_t secondOctet, const uint8_t thirdOctet, const uint8_t fourthOctet, const uint8_t fifthOctet, const uint8_t sixthOctet) const {
+    return _address.bytes[0] == firstOctet && _address.bytes[1] == secondOctet && _address.bytes[2] == thirdOctet && _address.bytes[3] == fourthOctet && _address.bytes[4] == fifthOctet && _address.bytes[5] == sixthOctet;
+  }
+  bool equals(const uint16_t firstWord, const uint16_t secondWord, const uint16_t thirdWord) const {
+    return _address.words[0] == firstWord && _address.words[1] == secondWord && _address.words[2] == thirdWord;
+  }
 
-  bool operator==(const MACAddress &addr) const {
+  bool operator==(const MACAddress& addr) const {
     return equals(addr);
   }
-  bool operator!=(const MACAddress &addr) const {
+  bool operator!=(const MACAddress& addr) const {
     return !equals(addr);
   }
 
-  bool operator==(const uint8_t *addr) const {
-    return _address.bytes[0] == addr[0] && _address.bytes[1] == addr[1] && _address.bytes[2] == addr[2] && _address.bytes[3] == addr[3] && _address.bytes[4] == addr[4] && _address.bytes[5] == addr[5];
+  bool operator==(const uint8_t* addr) const {
+    return equals(addr);
+  }
+  bool operator!=(const uint8_t* addr) const {
+    return !equals(addr);
+  }
+  bool operator==(const uint16_t* addr) const {
+    return equals(addr);
+  }
+  bool operator!=(const uint16_t* addr) const {
+    return !equals(addr);
   }
 
   uint8_t operator[](int index) const {
     return _address.bytes[index];
   }
-  uint8_t &operator[](int index) {
+  uint8_t& operator[](int index) {
     return _address.bytes[index];
   }
 
-  MACAddress &operator=(const uint8_t *address) {
-    memcpy(_address.bytes, address, sizeof(_address.bytes));
+  MACAddress& operator=(const uint8_t* addr) {
+    fromBytes(addr);
     return *this;
   }
-  MACAddress &operator=(const char *address) {
-    fromString(address);
+  MACAddress& operator=(const char* addr) {
+    fromString(addr);
+    return *this;
   }
-  MACAddress &operator=(const String &address) {
-    fromString(address);
+  MACAddress& operator=(const String& addr) {
+    fromString(addr);
+    return *this;
   }
-  operator uint8_t*()
-  {
-    return _address.bytes;
-  }
-  
 
-  uint8_t *toBytes() {
+  operator uint8_t*() {
+    return toBytes();
+  }
+  operator uint16_t*() {
+    return toWords();
+  }
+  operator String() {
+    return toString();
+  }
+
+  void setFormat(const MacFormat format) {
+    _format = format;
+  }
+  static void setDefaultFormat(const MacFormat format) {
+    _defaultFormat = format;
+  }
+
+  uint8_t* toBytes() {
     return _address.bytes;
+  }
+  uint16_t* toWords() {
+    return _address.words;
   }
   String toString() const {
-    char buf[28];
-    snprintf(buf, 20, "%02X:%02X:%02X:%02X:%02X:%02X", _address.bytes[0], _address.bytes[1], _address.bytes[2], _address.bytes[3], _address.bytes[4], _address.bytes[5]);
-    return String(buf);
+    char str[MAC_STR_LENGTH];
+    toCharArray(str);
+    return String(str);
+  }
+  int toCharArray(char* c_array) const {
+    switch(_format){
+      case MacFormat::UPPER_COLON:
+        return sprintf(c_array, MAC_FORMAT_UPPER_COLON, _address.bytes[0], _address.bytes[1], _address.bytes[2], _address.bytes[3], _address.bytes[4], _address.bytes[5]);
+        
+      case MacFormat::LOWER_COLON:
+        return sprintf(c_array, MAC_FORMAT_LOWER_COLON, _address.bytes[0], _address.bytes[1], _address.bytes[2], _address.bytes[3], _address.bytes[4], _address.bytes[5]);
+        
+      case MacFormat::UPPER_HYPHEN:
+        return sprintf(c_array, MAC_FORMAT_UPPER_HYPHEN, _address.bytes[0], _address.bytes[1], _address.bytes[2], _address.bytes[3], _address.bytes[4], _address.bytes[5]);
+        
+      case MacFormat::LOWER_HYPHEN:
+        return sprintf(c_array, MAC_FORMAT_LOWER_HYPHEN, _address.bytes[0], _address.bytes[1], _address.bytes[2], _address.bytes[3], _address.bytes[4], _address.bytes[5]);
+        
+      case MacFormat::UPPER_PERIOD:
+        return sprintf(c_array, MAC_FORMAT_UPPER_PERIOD, _address.words[0], _address.words[1], _address.words[2]);
+        
+      case MacFormat::LOWER_PERIOD:
+        return sprintf(c_array, MAC_FORMAT_LOWER_PERIOD, _address.words[0], _address.words[1], _address.words[2]);
+    }
+
+    return -1;
   }
 
-  virtual size_t printTo(Print &p) const {
-    String str = toString();
-    p.print(str);
-    return str.length();
+  virtual size_t printTo(Print& p) const {
+    char str[MAC_STR_LENGTH];
+    toCharArray(str);
+
+    return p.print(str);
+  }
+private:
+  void clear() {
+    memset(_address.bytes, 0, sizeof(_address.bytes));
   }
 };
 
